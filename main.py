11ff5e03c63d40625ee2f3234912058c18136987
Vincent Matinde <vincentmatinde@Vincents-MacBook-Air.local>
2023-01-27 19:06:37 +0300

    On net and off net visualization

--- main.py
+++ main.py
@@ -32,14 +32,14 @@ if tab == "Mobile Subscriptions":
     st.write("Total mobile subscriptions in the country. This corresponds to total of \
     all active mobile subscriptions in the country including both prepaid and postpaid subscriptions.")
     
-    fig = px.bar(df, x='Total Mobile (SIM) Subscriptions', y='Unnamed: 0', )
+    fig = px.bar(df, x='Total Mobile (SIM) Subscriptions', y='PERIOD', )
     st.plotly_chart(fig)
 
     st.title("Mobile Subscriptions by Operator in quarterly basis")
     st.write("The data here views the subscrition by operator in quarterly basis from 2017 to 2022.")
 
     operators_df = df[[
-                        'Unnamed: 0', 
+                        'PERIOD', 
                         'QUARTER',
                         'Safaricom SIM Subscription - Prepaid', 
                         'Safaricom SIM Subscription - Postpaid',
@@ -52,13 +52,13 @@ if tab == "Mobile Subscriptions":
                     ]]
 
 
-    period = st.selectbox("Select a period", operators_df['Unnamed: 0'].unique())
+    period = st.selectbox("Select a period", operators_df['PERIOD'].unique())
 
     # create two columns
     
 
     
-    operators_df_new = operators_df[operators_df['Unnamed: 0'] == period]
+    operators_df_new = operators_df[operators_df['PERIOD'] == period]
     if operators_df_new.empty:
         st.error("No data found for the selected period.")
     else:
@@ -78,7 +78,7 @@ if tab == "Mobile Subscriptions":
     with col1:
         
         year = str(year)
-        operators_df_new = operators_df[operators_df['Unnamed: 0'].str.contains('October - December ' + year)]
+        operators_df_new = operators_df[operators_df['PERIOD'].str.contains('October - December ' + year)]
         if operators_df_new.empty:
             st.error("No data found for the selected year.")
         else:
@@ -94,4 +94,44 @@ if tab == "Mobile Subscriptions":
         fig = px.pie(operators_df_new, values=operators_df_new.iloc[:, 0], names=operators_df_new.index, hole=.3)
         st.plotly_chart(fig)
 
-    #
+    # Create a line chart of the columns: Safaricom PLC On-net, Safaricom PLC Off-net, Airtel Kenya On-net, Airtel Kenya Off-net, Telkom Kenya On-net, Telkom Kenya Off-net, Equitel On-net, Equitel Off-net
+   
+    # create line chart of the data
+
+    st.title("Mobile on and off-net calls by Operator")
+    st.write("You can view the data of how many on and off-net calls per year various operators had.")
+   
+    net_calls_df = df[[
+                        'Safaricom PLC On-net',
+                        'Airtel On-net',
+                        'Telkom On-net',
+                        'Equitel On-net',
+                    ]]
+
+    net_calls_df.set_index(df['PERIOD'], inplace=True)
+
+     # reverse the index
+    net_calls_df = net_calls_df.iloc[::-1]
+        
+    # create a line chart of the data with the index of df as the x axis
+    fig3 = px.line(net_calls_df, labels={'value': 'Calls', 'variable': 'Operator', 'index': 'Period'}, 
+                    title="On-net Calls by Operator")
+    
+    st.plotly_chart(fig3)
+
+    offnet_calls_df = df[[
+                        'Safaricom PLC Off-net',
+                        'Airtel Off-net',
+                        'Telkom Off-net',
+                        'Equitel Off-net',
+                    ]]
+
+    offnet_calls_df.set_index(df['PERIOD'], inplace=True)
+
+    # reverse the index
+    offnet_calls_df = offnet_calls_df.iloc[::-1]
+    # create a line chart of the data with the index of df as the x axis
+    fig4 = px.line(offnet_calls_df, labels={'value': 'Calls', 'variable': 'Operator', 'index': 'Period'}, 
+                    title="Off-net Calls by Operator")
+    
+    st.plotly_chart(fig4)
